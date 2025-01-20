eb2f5b82024a44dc8cc64101f3ebb61cca55f5aa
Leonardo dos Santos Antunes <antunesleonardo623@gmail.com>
2025-01-20 14:52:49 -0300

    Projeto

---
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,5 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = document.getElementById("de").value;
+    let ate = document.getElementById("ate").value;
+}
