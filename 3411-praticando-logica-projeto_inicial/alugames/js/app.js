525bfc1e95463c1f7d11b7dd0f83f7c56021922a
Leonardo dos Santos Antunes <antunesleonardo623@gmail.com>
2025-01-31 14:07:20 -0300

    1

---
+++ 3411-praticando-logica-projeto_inicial/alugames/js/app.js
@@ -0,0 +1,5 @@
+function alterarStatus(id) {
+    let jogo = document.getElementById(`game-${id}`);
+    let imagem = document.querySelector('.dashboard__item__img');
+    let botao = document.querySelector('');
+}
